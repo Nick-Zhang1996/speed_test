8e4c32202f6ebb7c7716a402f5207c8c289535ff
Nick <zhangzyhack@126.com>
2014-07-26 10:04:00 +0800

    Initial Commit

---
+++ speed_test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  speed_test
+//
+//  Created by Nick zhang on 14-7-26.
+//  Copyright (c) 2014年 Nick Zhang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
